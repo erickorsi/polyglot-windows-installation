389521a831e4b140df3a9a5bec44fdeb12ea26af
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-06 14:33:06 -0300

    Wheels

--- get_dependencies.py
+++ get_dependencies.py
@@ -6,12 +6,74 @@ Created on Mon Sep 06 11:51 2022
 
 Script for downloading dependencies of the polyglot package.
 """
-
+import os
 import sys
 import requests
 
-response = requests.get(url = "https://www.lfd.uci.edu/~gohlke/pythonlibs",
-                           headers = {
-                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-                           })
+#--------------------------------------------------------------------------------
+def get_wheels(pyicu_version="2.9", pycld2_version="0.41"):
+    '''
+    Downloads the required wheels from https://www.lfd.uci.edu/~gohlke/pythonlibs/
+    according to the interpreter Python version.
 
+    Wheels Morfessor and futures are already downloaded, and work for any Python 3 version.
+
+    Parameters
+    ----------
+    pyicu_version : string, default="2.9"
+        Which version of the PyICU wheel you want to download.
+    pycld2_version : string, default="0.41"
+        Which version of the pycld2 wheel you want to download.
+
+    Returns
+    ----------
+    None.
+    May raise error in case of unsupported python version, wrong wheel version, or connection error.
+    Saves the wheels PyICU and pycld2 in the wheels forlder, located in the same directory as this script.
+    '''
+    # Versions
+    v = sys.version_info
+    if v.major == 2:
+        print("Only supported for python 3.")
+        raise SystemExit
+    v1 = str(v.major) + str(v.minor)
+    print("Python version {maj}.{min}.{mic}".format(maj=v.major,min=v.minor,mic=v.micro))
+    if sys.maxsize > 2**32:
+        v2 = "_amd64"
+        print("Running on 64bit processor.")
+    else:
+        v2 = "32"
+        print("Running on 32bit processor.")
+
+    # PyICU
+    print("Downloading wheel PyICU-{0}".format(pyicu_version))
+    try:
+        wheel = "PyICU-{pyicu}-cp{v1}-cp{v1}-win{v2}.whl".format(pyicu=pyicu_version,v1=v1,v2=v2)
+        response = requests.get(url = "https://download.lfd.uci.edu/pythonlibs/archived/"+wheel)
+        open(os.path.join(dir_path, wheel), "wb").write(response.content).close()
+        print("PyICU-{0} downloaded.".format(pyicu_version))
+    except Exception as e:
+        print("Unable to download PyICU:\n", e)
+        raise SystemExit
+
+    # pycld2
+    print("Downloading wheel pycld2-{0}").format(pycld2_version)
+    try:
+        wheel = "pycld2-{pycld2}-cp{v1}-cp{v1}-win{v2}.whl".format(pycld2=pycld2_version,v1=v1,v2=v2)
+        response = requests.get(url = "https://download.lfd.uci.edu/pythonlibs/archived/"+wheel)
+        open(os.path.join(dir_path, wheel), "wb").write(response.content).close()
+    except Exception as e:
+        print("Unable to download pycld2:\n", e)
+        raise SystemExit
+
+def install_wheels():
+    '''
+    '''
+    for wheel in dir_path.listdir():
+        os.system('ls -l')
+
+#--------------------------------------------------------------------------------
+# Wheels directory
+dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "wheels")
+get_wheels()
+install_wheels()
