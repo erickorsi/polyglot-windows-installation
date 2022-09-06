7aa0831670cac7220e2b415a1582193508ba6e43
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-06 18:17:51 -0300

    Setup finished

--- setup.bat
+++ setup.bat
@@ -1,3 +1,13 @@
+:: Install dependencies
+echo "Installing dependencies for polyglot..."
 pip3 install -U pip
 pip3 install -U requests
 python get_dependencies.py
+echo "Dependencies installed."
+:: Install polyglot
+echo "Installing polyglot..."
+git clone https://github.com/aboSamoor/polyglot.git
+cd polyglot
+python setup.py install
+cd ..
+RMDIR /S /Q %0\..\polyglot
